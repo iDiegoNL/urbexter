b2b3d8d87c0a136f9541b2701276c28cb22042a0
Diego <me@diegor.nl>
2022-02-10 18:52:33 +0100

    Fix typo in migration

--- database/migrations/2022_02_10_110303_add_location_status_id_to_locations_table.php
+++ database/migrations/2022_02_10_110303_add_location_status_id_to_locations_table.php
@@ -19,7 +19,7 @@ class AddLocationStatusIdToLocationsTable extends Migration
                 ->after('reconverted_year')
                 ->constrained()
                 ->restrictOnUpdate()
-                ->restrictOnUpdate();
+                ->restrictOnDelete();
         });
     }
 
