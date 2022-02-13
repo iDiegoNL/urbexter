6db5b98db0f4dd237a4e7de35d62a0130b82e15f
Diego <me@diegor.nl>
2022-02-13 03:28:37 +0100

    Make full location card on homepage clickable

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -12,22 +12,22 @@
     <div class="relative max-w-7xl mx-auto pt-8">
         <div class="grid gap-5 lg:grid-cols-3 lg:max-w-none">
             @foreach($locations as $location)
-                <x-app-ui::card
-                    :image="$location->getFirstMediaUrl('default', 'thumb') ?: 'https://via.placeholder.com/397x223?text=No+image+available'"
-                    :imageAlt="$location->name"
-                >
-                    <x-slot name="heading">
-                        <a href="{{ route('locations.show', $location->id) }}">
+                <a href="{{ route('locations.show', $location->id) }}">
+                    <x-app-ui::card
+                        :image="$location->getFirstMediaUrl('default', 'thumb') ?: 'https://via.placeholder.com/397x223?text=No+image+available'"
+                        :imageAlt="$location->name"
+                    >
+                        <x-slot name="heading">
                             {{ Spatie\Emoji\Emoji::countryFlag($location->country) }} {{ $location->name }}
-                        </a>
-                    </x-slot>
+                        </x-slot>
 
-                    <x-slot name="subheading">
-                        <p class="break-words">
-                            {{ Str::limit($location->description) }}
-                        </p>
-                    </x-slot>
-                </x-app-ui::card>
+                        <x-slot name="subheading">
+                            <p class="break-words">
+                                {{ Str::limit($location->description) }}
+                            </p>
+                        </x-slot>
+                    </x-app-ui::card>
+                </a>
             @endforeach
         </div>
     </div>
